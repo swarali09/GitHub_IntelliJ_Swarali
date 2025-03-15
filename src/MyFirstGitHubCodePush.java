566f7cd280dbed8b77a8c1560f7d6dd0e478ed3e
swarali09 <swarali.shirodkar@gmail.com>
2025-03-15 20:14:48 +0530

    Init Commit

---
+++ src/MyFirstGitHubCodePush.java
@@ -0,0 +1,6 @@
+public class MyFirstGitHubCodePush {
+    public static void main(String[] args) {
+        System.out.println("Yeyyy Code push ho gayaaa!!!!");
+        System.out.println("Yeyyy Code push ho maja aa gayaa!!!!!!");
+    }
+}
